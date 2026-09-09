38637006e14790da0901565d353c36c2fb52bae6
abdelrahman nassar <atef18008@gmail.com>
2026-09-09 00:34:45 -0700

    v3 last check

--- src/rfqs/rfqs.module.ts
+++ src/rfqs/rfqs.module.ts
@@ -1,4 +1,4 @@
-import { Module } from '@nestjs/common';
+import { forwardRef, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { NumberingService } from '../common/services/numbering.service';
 import { ItemsModule } from '../items/items.module';
@@ -8,7 +8,7 @@ import { RfqsController } from './rfqs.controller';
 import { RfqsService } from './rfqs.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Rfq, RfqLine]), ItemsModule],
+  imports: [TypeOrmModule.forFeature([Rfq, RfqLine]), forwardRef(() => ItemsModule)],
   controllers: [RfqsController],
   providers: [RfqsService, NumberingService],
   exports: [RfqsService],
